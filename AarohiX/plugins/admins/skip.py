57adce47e4e70e58ecb1c0f3b4331275861905e6
Team Freedom <167733685+fps90@users.noreply.github.com>
2024-04-29 17:11:23 +0300

    Update skip.py

--- AarohiX/plugins/admins/skip.py
+++ AarohiX/plugins/admins/skip.py
@@ -64,7 +64,7 @@ async def skip(cli, message: Message, _, chat_id):
             f"~︙عزيزي {message.from_user.mention} \n~︙عليك الأشتراك في قناة البوت \n~︙قناة البوت : @{Muntazer}.", 
             disable_web_page_preview=True, 
             reply_markup=InlineKeyboardMarkup([ 
-                [InlineKeyboardButton("< Source Plus >", url=link)] 
+                [InlineKeyboardButton("< Link >", url=link)] 
             ]) 
         ) 
         return 
