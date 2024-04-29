051fcbbd933174bf01aa2dbbabc361f859cc3d28
Team Freedom <167733685+fps90@users.noreply.github.com>
2024-04-29 17:48:35 +0300

    Update play.py

--- AarohiX/plugins/play/play.py
+++ AarohiX/plugins/play/play.py
@@ -1,3 +1,7 @@
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from ast import ExceptHandler
+from pyrogram import filters
+from pyrogram.types import (InlineKeyboardMarkup, InputMediaPhoto,
 import random
 import string
 from strings.filters import command
